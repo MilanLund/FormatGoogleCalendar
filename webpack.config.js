3134b1b82a9b032a87c9c731756a66f74ca48b26
Milan Lund <hello@milanlund.com>
2017-09-28 11:18:03 +0200

    Removed minified map

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
             include: /\.min\.js$/,
             sourceMap: true,
             parallel: true,
-            sourceMap: true
+            sourceMap: false
         })
     ]  
 };
